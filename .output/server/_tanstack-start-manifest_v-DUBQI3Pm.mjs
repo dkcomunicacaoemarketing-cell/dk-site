9296a4881b7785dfee7cd999d0705e6b93bac366
jeanschwarz <jeanschwarzfilmdirector@gmail.com>
2026-07-31 12:03:45 -0300

    fix: corrige estrutura de pastas

---
+++ .output/server/_tanstack-start-manifest_v-DUBQI3Pm.mjs
@@ -0,0 +1,20 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-DUBQI3Pm.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "/Users/jeanschwarz/Downloads/dk_site/src/routes/__root.tsx",
+		children: ["/"],
+		preloads: ["/assets/index-CJsDZSv1.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-CJsDZSv1.js"
+		} }]
+	},
+	"/": {
+		filePath: "/Users/jeanschwarz/Downloads/dk_site/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-a6SZpv5y.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
